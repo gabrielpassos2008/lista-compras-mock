d6b6bcde1171aef82c61a833fe97abbc97511bc0
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-04-08 21:46:43 -0300

    - adicionei o teste para o delete

--- backend/tests/test_routes_itens.py
+++ backend/tests/test_routes_itens.py
@@ -24,4 +24,12 @@ class TestRotasPostCadastroItem:
             "id": 1, "name": "feijão", "quantity":2, "purchased": True
         }
         mock_svc.create_item.assert_called_once()
-            
+        
+@patch("app.routes.items.item_service")       
+class TestRotasDeleteItemPorId:
+    def test_delete_item_por_id(self, mock_svc,client):
+        mock_svc.delete_item.return_value = ShoppingItem(id=2, name="carne", quantity=1, purchased= True)
+        resp = client.delete("api/items/2")
+        assert resp.status_code == 204
+
+        mock_svc.delete_item.assert_called_once()
