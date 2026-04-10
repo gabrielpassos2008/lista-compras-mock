4318807ae72c10c99668a2f7f51e0afded60542a
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-04-09 21:09:45 -0300

    -adicionei novo teste parar erros 404 e 400

--- backend/tests/test_routes_itens.py
+++ backend/tests/test_routes_itens.py
@@ -1,5 +1,6 @@
 from unittest.mock import patch
-from app.models.item import ShoppingItem
+from app.models.item import ShoppingItem 
+from app.services.item_service import ItemValidationError
 
 
 @patch("app.routes.items.item_service")
@@ -14,9 +15,15 @@ class TestRotasListagemItensPorId:
         }
         mock_svc.get_item.assert_called_once()
         
+    def test_get_listagem_itens_nao_existente_por_id_mock(self, mock_svc,client):
+        mock_svc.get_item.return_value = None
+        resp = client.get("/api/items/1")
+        assert resp.status_code == 404
+        mock_svc.get_item.assert_called_once()
+        
 @patch("app.routes.items.item_service")
 class TestRotasPostCadastroItem:
-    def test_post_cadastrar_item(self, mock_svc, client):
+    def test_post_cadastrar_item_mock(self, mock_svc, client):
         mock_svc.create_item.return_value = ShoppingItem(id=1, name="feijão", quantity=2, purchased= True)
         resp = client.post("api/items")
         assert resp.status_code == 201
@@ -24,16 +31,22 @@ class TestRotasPostCadastroItem:
             "id": 1, "name": "feijão", "quantity":2, "purchased": True
         }
         mock_svc.create_item.assert_called_once()
+    
+    def test_post_cadastrar_item_erro_mock(self,mock_svc,client):
+        mock_svc.create_item.side_effect = ItemValidationError("O nome do item é obrigatório.", code="name_required")
+        resp = client.post("api/items")
+        assert resp.status_code == 400
+        mock_svc.create_item.assert_called_once()
         
 @patch("app.routes.items.item_service")       
 class TestRotasDeleteItemPorId:
-    def test_delete_item_por_id(self, mock_svc,client):
+    def test_delete_item_por_id_mock(self, mock_svc,client):
         mock_svc.delete_item.return_value = ShoppingItem(id=2, name="carne", quantity=1, purchased= True)
         resp = client.delete("api/items/2")
         assert resp.status_code == 204
         mock_svc.delete_item.assert_called_once()
         
-    def test_delete_item_nao_existente_por_id(self,mock_svc,client):
+    def test_delete_item_nao_existente_por_id_mock(self,mock_svc,client):
         mock_svc.delete_item.return_value = None
         resp = client.delete("api/items/90")
         assert resp.status_code == 404
